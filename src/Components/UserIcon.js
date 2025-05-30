b26afd574996fdc1fd6c414724b2a60836b41a93
PayalRoy2004 <payal2004@gmail.com>
2025-05-30 11:48:24 +0530

    userIcon div changes

--- src/Components/UserIcon.js
+++ src/Components/UserIcon.js
@@ -3,7 +3,7 @@ import React from 'react'
 const UserIcon = () => {
   return (
     <div>
-      <img className="h-11 w-20 mx-[7rem] ml-[34rem] my-2" src='https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png' alt='user icon'/>
+      <img className="h-11 w-20 mx-[1rem] ml-[40.75rem] my-2" src='https://static.vecteezy.com/system/resources/previews/019/879/186/original/user-icon-on-transparent-background-free-png.png' alt='user icon'/>
     </div>
   )
 }
